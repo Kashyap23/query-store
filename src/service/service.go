c051b1bca5ad5cd9fe21cd97e9dae762e2619d9a
Kashyap23 <avilakashyap97@gmail.com>
2021-03-06 19:43:12 +0530

    Code cleanup

--- src/service/service.go
+++ src/service/service.go
@@ -1,109 +1,109 @@
 package service
 
 import (
-	"log"
-	"strings"
-	"strconv"
+	"github.com/Kashyap23/query-store/src/utils"
 	"io/ioutil"
-    "github.com/Kashyap23/query-store/src/utils"
+	"log"
+	"strconv"
+	"strings"
 )
 
 // Name of the dir to store the queries
 var queryDir = "query-store"
 
 type QueryStore struct {
-    Dir string
+	Dir string
 }
 
 var queryStore QueryStore
 
 // Initialize query store
-func InitQueryStore(dirPath string)(QueryStore){
-    dir := QueryStore{}
-    err := utils.CreateDir(dirPath)
-    if err != nil {
-        log.Panic(err)
-    }
-    dir.Dir = dirPath
-    return dir
+func InitQueryStore(dirPath string) QueryStore {
+	dir := QueryStore{}
+	err := utils.CreateDir(dirPath)
+	if err != nil {
+		log.Panic(err)
+	}
+	dir.Dir = dirPath
+	return dir
 }
 
 // Store query
-func (q *QueryStore) Set(query string)(string){
-    // Validate if dir exists
-    if ok := utils.CheckIfPathExists(q.Dir); !ok {
-	    utils.CreateDir(queryDir)
-    }
-    // Store to file
-    hash, err := utils.GenerateRandomHash()
-    if err != nil {
-        log.Println("Error generating random hash - ", err)
-        panic(err)
-    }
-    filePath := q.Dir + "/" + hash
+func (q *QueryStore) Set(query string) string {
+	// Validate if dir exists
+	if ok := utils.CheckIfPathExists(q.Dir); !ok {
+		utils.CreateDir(queryDir)
+	}
+	// Store to file
+	hash, err := utils.GenerateRandomHash()
+	if err != nil {
+		log.Println("Error generating random hash - ", err)
+		panic(err)
+	}
+	filePath := q.Dir + "/" + hash
 
-    err = ioutil.WriteFile(filePath, []byte(query), 0755)
-    if err != nil {
-	    log.Println("Error writing to file - ", err)
-	    return ""
-    }
+	err = ioutil.WriteFile(filePath, []byte(query), 0755)
+	if err != nil {
+		log.Println("Error writing to file - ", err)
+		return ""
+	}
 
-    log.Println("Wrote query to file successfully - ", hash)
-    return hash
+	log.Println("Wrote query to file successfully - ", hash)
+	return hash
 }
 
 // Retreive query
-func (q *QueryStore) Get(key string)(string){
-    // Check if file exists
-    filePath := q.Dir + "/" + key
-    if ok := utils.CheckIfPathExists(filePath); !ok {
-	    log.Println("Path - ", filePath, " doesn't exist")
-	    return ""
-    }
+func (q *QueryStore) Get(key string) string {
+	// Check if file exists
+	filePath := q.Dir + "/" + key
+	if ok := utils.CheckIfPathExists(filePath); !ok {
+		log.Println("Path - ", filePath, " doesn't exist")
+		return ""
+	}
 
-    data, err := ioutil.ReadFile(filePath)
-    if err != nil {
-	    log.Println("Error reading file - ", filePath, " error - ", err)
-	    return ""
-    }
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Println("Error reading file - ", filePath, " error - ", err)
+		return ""
+	}
 
-    return string(data)
+	return string(data)
 }
 
-func EncodeByteToString(body []byte)(string){
-    text := ""
-    for _, i := range body {
+func EncodeByteToString(body []byte) string {
+	text := ""
+	for _, i := range body {
 		c := strconv.Itoa(int(i))
 		text = text + c + "_"
-    }
+	}
 
-    text = text[0:len(text)-1]
+	text = text[0 : len(text)-1]
 
-    return text
+	return text
 }
 
-func DecodeStringToByte(text string)([]byte){
-    _bytes := []byte{}
-    splitText := strings.Split(text, "_")
-    for _, s := range splitText {
+func DecodeStringToByte(text string) []byte {
+	_bytes := []byte{}
+	splitText := strings.Split(text, "_")
+	for _, s := range splitText {
 		i, err := strconv.Atoi(s)
 		if err != nil {
 			panic(err)
 		}
 		_bytes = append(_bytes, []byte(string(i))...)
-    }
+	}
 
-    log.Println("Original string - ", string(_bytes))
+	log.Println("Original string - ", string(_bytes))
 
-    return _bytes
+	return _bytes
 }
 
-func StoreQuery(query []byte)(string){
-    encodedQuery := EncodeByteToString(query)
-    return queryStore.Set(encodedQuery)
+func StoreQuery(query []byte) string {
+	encodedQuery := EncodeByteToString(query)
+	return queryStore.Set(encodedQuery)
 }
 
-func GetQueryForHash(key string)([]byte){
-    query := queryStore.Get(key)
-    return DecodeStringToByte(query)
+func GetQueryForHash(key string) []byte {
+	query := queryStore.Get(key)
+	return DecodeStringToByte(query)
 }
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,44 +1,44 @@
 package utils
 
 import (
-    "os"
-    "encoding/hex"
-    "log"
-    "math/rand"
-    "errors"
+	"encoding/hex"
+	"errors"
+	"log"
+	"math/rand"
+	"os"
 )
 
 // Create new dir for the path
-func CreateDir(path string)(error){
-    err := os.Mkdir(path, 0755)
-    if err != nil {
-        err = errors.New("Error creating query-store dir - " + err.Error())
-	    log.Println(err)
-        return err
-    }
+func CreateDir(path string) error {
+	err := os.Mkdir(path, 0755)
+	if err != nil {
+		err = errors.New("Error creating query-store dir - " + err.Error())
+		log.Println(err)
+		return err
+	}
 
-    log.Println("Created dir - ", path, " successfully")
-    return nil
+	log.Println("Created dir - ", path, " successfully")
+	return nil
 }
 
 // Generate hash
-func GenerateRandomHash()(string, error) {
-    b := make([]byte, 32)
-    if _, err := rand.Read(b); err != nil {
-        log.Println("Error generating hash - ", err)
-        return "", err
-    }
+func GenerateRandomHash() (string, error) {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		log.Println("Error generating hash - ", err)
+		return "", err
+	}
 
-    return hex.EncodeToString(b), nil
+	return hex.EncodeToString(b), nil
 }
 
 // Check if if given path exists
-func CheckIfPathExists(path string)(bool){
-    _, err := os.Stat(path)
-    if err != nil {
+func CheckIfPathExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
 		log.Println("Error checking for path - ", err)
 		return false
-    }
+	}
 
-    return true
+	return true
 }
